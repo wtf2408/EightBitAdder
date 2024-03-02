13d2f13a758b4a89c3b6016f14654dbf4f13f09c
wtf2408 <bogdanevseyv@yandex.ru>
2024-03-03 01:06:04 +0300

    Добавьте файлы проекта.

---
+++ Adder.cs
@@ -0,0 +1,103 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BitAdder
+{
+    public class Adder : INotifyPropertyChanged
+    {
+        private bool signMode { get; set; }
+        #region FirstNumber
+        byte firstNumber;
+        public byte FirstNumber 
+        { 
+            get { return firstNumber; }
+            set { firstNumber = value; OnPropertyChanged(nameof(FirstNumber)); } 
+        }
+        public sbyte SignFirstNumber
+        {
+            get { return (sbyte)firstNumber; }
+            set { FirstNumber = (byte)value; OnPropertyChanged(nameof(SignFirstNumber)); }
+        }
+        #endregion
+
+        #region SecondNumber
+        byte secondNumber;
+        public byte SecondNumber
+        {
+            get { return secondNumber; }
+            set { secondNumber = value; OnPropertyChanged(nameof(SecondNumber)); }
+        }
+        public sbyte SignSecondNumber
+        {
+            get { return (sbyte)secondNumber; }
+            set { SecondNumber = (byte)value; OnPropertyChanged(nameof(SignSecondNumber)); }
+        }
+        #endregion
+
+        #region Result
+        byte result;
+        public byte Result
+        {
+            get { return result; }
+            set { result = value; OnPropertyChanged(nameof(Result)); }
+        }
+        public sbyte SignResult
+        {
+            get { return (sbyte)result; }
+            set { Result = (byte)value; OnPropertyChanged(nameof(SignResult)); }
+        }
+        #endregion
+
+        public void Add()
+        {
+            SignResult = (sbyte)(firstNumber + secondNumber);
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public void OnSwitch(object sender, RoutedEventArgs e)
+        {
+            var stackPanel = (StackPanel)((CheckBox)sender).Parent;
+            var switches = stackPanel.Children;
+            sbyte tempNumber = 0;
+            for (int i = 0; i < switches.Count; i++)
+            {
+                if ( ((CheckBox)switches[switches.Count - i - 1]).IsChecked == true )
+                {
+                    tempNumber += (sbyte)Math.Pow(2, i);
+                }
+            }
+            if (stackPanel.Name.Contains("First")) SignFirstNumber = tempNumber;
+            else if (stackPanel.Name.Contains("Second")) SignSecondNumber = tempNumber;
+            else if (stackPanel.Name.Contains("Result")) SignResult = tempNumber;
+        }
+
+        public string MinValue 
+        { 
+            get
+            {
+                if (signMode) return sbyte.MinValue.ToString();
+                return byte.MinValue.ToString();
+            } 
+        }
+        public string MaxValue
+        {
+            get
+            {
+                if (signMode) return sbyte.MaxValue.ToString();
+                return byte.MaxValue.ToString();
+            }
+        }
+        public void SetSignMode(bool signMode)
+        {
+            this.signMode = signMode;
+            OnPropertyChanged(nameof(MinValue));
+            OnPropertyChanged(nameof(MaxValue));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BitAdder
+{
+    public partial class MainWindow : Window
+    {
+        private Adder adder;
+        Brush signedBrush;
+        Brush unsignedBrush;
+        #region Bindings
+        Binding firstNumberSignBinding;
+        Binding firstNumberBinding;
+        Binding secondNumberSignBinding;
+        Binding secondNumberBinding;
+        Binding resultSignBinding;
+        Binding resultBinding;
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            adder = (Adder)DataContext;
+            adder.SetSignMode(false);
+            adder.PropertyChanged += ChangeSwitchPanel;
+
+            unsignedBrush = ((CheckBox)FirstNumberSwitchPanel.Children[0]).Background;
+            signedBrush = Brushes.DeepPink;
+
+            #region Bindings initializing 
+            firstNumberSignBinding = new Binding("SignFirstNumber") { UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged }; 
+            secondNumberSignBinding = new Binding("SignSecondNumber") { UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged };
+            resultSignBinding = new Binding("SignResult") { UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged };
+
+
+            firstNumberBinding = new Binding("FirstNumber") { UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged };
+            secondNumberBinding = new Binding("SecondNumber") { UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged };
+            resultBinding = new Binding("Result") { UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged };
+            #endregion
+
+            foreach (var switcher in FirstNumberSwitchPanel.Children) ((CheckBox)switcher).Click += adder.OnSwitch;
+            foreach (var switcher in SecondNumberSwitchPanel.Children) ((CheckBox)switcher).Click += adder.OnSwitch;
+            foreach (var switcher in ResultNumberSwitchPanel.Children) ((CheckBox)switcher).Click += adder.OnSwitch;
+
+        }
+
+
+
+        private void SignModeSwitch(object sender, RoutedEventArgs e)
+        {
+            if (((CheckBox)sender).IsChecked == true)
+            {
+                FirstNumberTextBox.SetBinding(TextBox.TextProperty, firstNumberSignBinding);
+                SecondNumberTextBox.SetBinding(TextBox.TextProperty, secondNumberSignBinding);
+                ResultTextBlock.SetBinding(TextBlock.TextProperty, resultSignBinding);
+                adder.SetSignMode(true);
+                ((CheckBox)FirstNumberSwitchPanel.Children[0]).Background = signedBrush;
+                ((CheckBox)SecondNumberSwitchPanel.Children[0]).Background = signedBrush;
+                ((CheckBox)ResultNumberSwitchPanel.Children[0]).Background = signedBrush;
+            }
+            else
+            {
+                FirstNumberTextBox.SetBinding(TextBox.TextProperty, firstNumberBinding);
+                SecondNumberTextBox.SetBinding(TextBox.TextProperty, secondNumberBinding);
+                ResultTextBlock.SetBinding(TextBlock.TextProperty, resultBinding);
+                adder.SetSignMode(false);
+                ((CheckBox)FirstNumberSwitchPanel.Children[0]).Background = unsignedBrush;
+                ((CheckBox)SecondNumberSwitchPanel.Children[0]).Background = unsignedBrush;
+                ((CheckBox)ResultNumberSwitchPanel.Children[0]).Background = unsignedBrush;
+            }
+
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            adder.Add();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void ChangeSwitchPanel(object? sender, PropertyChangedEventArgs e)
+        {
+            string number;
+            UIElementCollection switches;
+            
+            if (e.PropertyName.Contains("First"))
+            {
+                switches = FirstNumberSwitchPanel.Children;
+                number = ((Adder)sender).FirstNumber.ToString("B8");
+
+                for (int i = 0;  i < number.Length; i++)
+                {
+                    if (number[number.Length-i-1] == '1') ((CheckBox)switches[switches.Count-i-1]).IsChecked = true;
+                    else ((CheckBox)FirstNumberSwitchPanel.Children[switches.Count-i-1]).IsChecked = false;
+                }
+            }
+            else if (e.PropertyName.Contains("Second"))
+            {
+                switches = SecondNumberSwitchPanel.Children;
+                number = ((Adder)sender).SecondNumber.ToString("B8");
+
+                for (int i = 0; i < number.Length; i++)
+                {
+                    if (number[number.Length-i-1] == '1') ((CheckBox)switches[switches.Count-i-1]).IsChecked = true;
+                    else ((CheckBox)switches[switches.Count-i-1]).IsChecked = false;
+                }
+            }
+            else if (e.PropertyName.Contains("Result"))
+            {
+                switches = ResultNumberSwitchPanel.Children;
+                number = ((Adder)sender).Result.ToString("B8");
+                for (int i = 0; i < number.Length; i++)
+                {
+                    if (number[number.Length-i-1] == '1') ((CheckBox)switches[switches.Count-i-1]).IsChecked = true;
+                    else ((CheckBox)switches[switches.Count-i-1]).IsChecked = false;
+                }
+            }
+        }   
+    }
+}
